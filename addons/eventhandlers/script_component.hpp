cf1e829d8faf87987424b1f2a81537deb3b0314c
Jonas Hoffmann <hoffmann.jonas95@gmail.com>
2019-12-28 16:57:22 +0100

    cleanup and Fix Macros

--- addons/eventhandlers/script_component.hpp
+++ addons/eventhandlers/script_component.hpp
@@ -5,11 +5,11 @@
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 
-#ifdef DEBUG_ENABLED_WP
+#ifdef DEBUG_ENABLED_EVENTHANDLERS
     #define DEBUG_MODE_FULL
 #endif
-    #ifdef DEBUG_SETTINGS_WP
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_WP
+    #ifdef DEBUG_SETTINGS_EVENTHANDLERS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_EVENTHANDLERS
 #endif
 
 #include "\z\lambs\addons\main\script_macros.hpp"
