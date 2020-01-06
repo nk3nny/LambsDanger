f461b76f7437aefb44b366a0a862468c1912d3ed
Jonas Hoffmann <hoffman.jonas95@gmail.com>
2020-01-06 06:58:33 +0100

    do small Spacing Cleanup

--- addons/wp/config.cpp
+++ addons/wp/config.cpp
@@ -1,15 +1,15 @@
 #include "script_component.hpp"
-    class CfgPatches {
-        class ADDON {
-            name = COMPONENT_NAME;
-            units[] = {};
-            weapons[] = {};
-            requiredVersion = REQUIRED_VERSION;
-            requiredAddons[] = {"lambs_danger"};
-            author = "LAMBS Dev Team";
-            VERSION_CONFIG;
-        };
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"lambs_danger"};
+        author = "LAMBS Dev Team";
+        VERSION_CONFIG;
     };
+};
 
 
 #include "CfgEventHandlers.hpp"
