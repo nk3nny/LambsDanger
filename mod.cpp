a7aa8aaf852fca8eeab7a535100f157098699569
nkenny <nk3nny@gmail.com>
2020-02-19 13:45:28 +0100

    - Updated mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,3 +1,15 @@
 name = "LAMBS Danger.fsm";
 dir = "@lambs";
 author = "LAMBS Dev Team";
+
+tooltipOwned = "LAMBS Danger.fsm";
+description = "LAMBS Danger.fsm";
+picture = "lambs_logo.paa";
+logoSmall = "lambs_logo.paa";
+logo = "lambs_logo.paa";
+logoOver = "lambs_logo.paa";
+hidePicture = 0;
+hideName = 0;
+actionName = "Website";
+action = "http://www.nopryl.no/";
+overview = "A considerable expansion of vanilla Danger.fsm. Added are features which enhance AI responses and reactions to fire. ";
