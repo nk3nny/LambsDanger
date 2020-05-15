795db46a7b60b9c534ac6c744b6ea4a8e05b631a
Jonas Hoffmann <hoffmann.jonas95@gmail.com>
2020-05-15 17:04:14 +0200

    Split Search and normal modules
    add Icons
    Add L10N
    Update icon for Task reset

--- addons/danger/ZEN_CfgContext.hpp
+++ addons/danger/ZEN_CfgContext.hpp
@@ -2,39 +2,36 @@ class ZEN_context_menu_actions {
     class ADDON {
         displayName = COMPONENT_NAME;
         condition = QUOTE(!((_groups isEqualTo []) && (_objects isEqualTo [])));
-        icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
         priority = 5;
         class EnableAI {
-            displayName = "Enable AI";
+            displayName = CSTRING(Context_EnableAI);
             statement = QUOTE([ARR_2(_objects,_args)] call FUNC(setDisableAI));
             condition = QUOTE([ARR_2(_objects,_args)] call FUNC(showSetDisableAI));
-            icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
             args = 0;
         };
         class DisableAI: EnableAI {
-            displayName = "Disable AI";
+            displayName = CSTRING(Context_DisableAI);
             args = 1;
         };
         class EnableGroupAI {
-            displayName = "Disable Group AI";
+            displayName = CSTRING(Context_EnableGroupAI);
             statement = QUOTE([ARR_2(_groups,_args)] call FUNC(setDisableGroupAI));
             condition = QUOTE([ARR_2(_groups,_args)] call FUNC(showSetDisableGroupAI));
-            icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
             args = 0;
         };
         class DisableGroupAI: EnableGroupAI {
-            displayName = "Disable Group AI";
+            displayName = CSTRING(Context_DisableGroupAI);
             args = 1;
         };
         class HasRadio {
-            displayName = "Has Radio";
+            displayName = CSTRING(Context_HasRadio);
             statement = QUOTE([ARR_2(_objects,_args)] call FUNC(setHasRadio));
             condition = QUOTE([ARR_2(_objects,_args)] call FUNC(showHasRadio));
-            icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
+            icon = "\a3\ui_f\data\IGUI\Cfg\simpleTasks\types\radio_ca.paa";
             args = 1;
         };
         class HasNoRadio: HasRadio {
-            displayName = "Has No Radio";
+            displayName = CSTRING(Context_HasNoRadio);
             args = 0;
         };
     };
--- addons/wp/ZEN_CfgContext.hpp
+++ addons/wp/ZEN_CfgContext.hpp
@@ -1,66 +1,68 @@
-class ZEN_context_menu_actions {
+ class ZEN_context_menu_actions {
     class ADDON {
-        displayName = COMPONENT_NAME;
-        icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
-        priority = 4;
+        displayName = "LAMBS Danger Waypoints";
+        priority = 3;
         class CreateTarget {
-            displayName = "Create Target";
+            displayName = CSTRING(Context_CreateTarget);
             statement = QUOTE([ARR_2(_objects, _position)] call FUNC(setTarget));
-            icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
+            icon = "\a3\3den\Data\CfgWaypoints\destroy_ca.paa";
         };
         class TaskArtilleryRegister {
-            displayName = "Task Register Artillery";
+            displayName = CSTRING(Module_TaskArtilleryRegister_DisplayName);
             condition = QUOTE(!((_groups isEqualTo []) && (_objects isEqualTo [])));
             statement = QUOTE([ARR_2(_groups, _objects)] call FUNC(setArtilleryRegister));
             icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
         };
         class TaskCamp {
-            displayName = "Task Camp";
+            displayName = CSTRING(Module_TaskCamp_DisplayName);
             condition = QUOTE(!((_groups isEqualTo []) && (_objects isEqualTo [])));
             statement = QUOTE([ARR_2(_groups, _objects)] call FUNC(setCamp));
-            icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
+            icon = "\a3\3DEN\Data\CfgWaypoints\Guard_ca.paa";
         };
         class TaskCQB {
-            displayName = "Task CQB";
+            displayName = CSTRING(Module_TaskCQB_DisplayName);
             condition = QUOTE(!((_groups isEqualTo []) && (_objects isEqualTo [])));
             statement = QUOTE([ARR_2(_groups, _objects)] call FUNC(setCQB));
-            icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
+            icon = "\a3\3DEN\Data\CfgWaypoints\Scripted_ca.paa";
         };
-        class TaskCreep {
-            displayName = "Task Creep";
-            condition = QUOTE(!((_groups isEqualTo []) && (_objects isEqualTo [])));
-            statement = QUOTE([ARR_2(_groups, _objects)] call FUNC(setCreep));
-            icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
-        };
+
         class TaskGarrison {
-            displayName = "Task Garrison";
+            displayName = CSTRING(Module_TaskGarrison_DisplayName);
             condition = QUOTE(!((_groups isEqualTo []) && (_objects isEqualTo [])));
             statement = QUOTE([ARR_2(_groups, _objects)] call FUNC(setGarrison));
-            icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
-        };
-        class TaskHunt {
-            displayName = "Task Hunt";
-            condition = QUOTE(!((_groups isEqualTo []) && (_objects isEqualTo [])));
-            statement = QUOTE([ARR_2(_groups, _objects)] call FUNC(setHunt));
-            icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
+            icon = "\a3\3DEN\Data\CfgWaypoints\Guard_ca.paa";
         };
         class TaskPatrol {
-            displayName = "Task Patrol";
+            displayName = CSTRING(Module_TaskPatrol_DisplayName);
             condition = QUOTE(!((_groups isEqualTo []) && (_objects isEqualTo [])));
             statement = QUOTE([ARR_2(_groups, _objects)] call FUNC(setPatrol));
-            icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
+            icon = "\A3\3DEN\Data\CfgWaypoints\Loiter_ca.paa";
         };
         class TaskReset {
-            displayName = "Task Reset";
+            displayName = CSTRING(Module_TaskReset_DisplayName);
             condition = QUOTE(!((_groups isEqualTo []) && (_objects isEqualTo [])));
             statement = QUOTE([ARR_2(_groups, _objects)] call FUNC(setReset));
-            icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
+            icon = "\a3\3DEN\Data\CfgWaypoints\cycle_ca.paa";
+        };
+    };
+    class DOUBLE(ADDON,Search) {
+        displayName = "LAMBS Danger Search Waypoints";
+        condition = QUOTE(!((_groups isEqualTo []) && (_objects isEqualTo [])));
+        priority = 4;
+        class TaskCreep {
+            displayName = CSTRING(Module_TaskCreep_DisplayName);
+            statement = QUOTE([ARR_2(_groups, _objects)] call FUNC(setCreep));
+            icon = "\a3\3DEN\Data\CfgWaypoints\Sentry_ca.paa";
+        };
+        class TaskHunt {
+            displayName = CSTRING(Module_TaskHunt_DisplayName);
+            statement = QUOTE([ARR_2(_groups, _objects)] call FUNC(setHunt));
+            icon = "\a3\3DEN\Data\CfgWaypoints\Sentry_ca.paa";
         };
         class TaskRush {
-            displayName = "Task Rush";
-            condition = QUOTE(!((_groups isEqualTo []) && (_objects isEqualTo [])));
+            displayName = CSTRING(Module_TaskRush_DisplayName);
             statement = QUOTE([ARR_2(_groups, _objects)] call FUNC(setRush));
-            icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\intel_ca.paa";
+            icon = "\a3\3DEN\Data\CfgWaypoints\Sentry_ca.paa";
         };
     };
 };
