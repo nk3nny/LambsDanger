c618d7f1a9f24271dd338ea1a7fae9ba913f6877
jokoho482 <hoffmann.jonas95@gmail.com>
2021-05-07 20:48:20 +0200

    Move Stuff to Stringtables

--- addons/wp/CfgWaypoints.hpp
+++ addons/wp/CfgWaypoints.hpp
@@ -2,67 +2,67 @@ class CfgWaypoints {
     class LAMBS_DangerAI {
         displayName = "Advanced AI [LAMBS]";
         class EGVAR(danger,Attack) {
-            displayName = "0 TASK ASSAULT";
+            displayName = CSTRING(Waypoint_TaskAssault_displayName);
             displayNameDebug = QEGVAR(danger,Attack);
             file = QPATHTOF(scripts\fnc_wpAssault.sqf);
             icon = "\a3\ui_f\data\GUI\Cfg\CommunicationMenu\attack_ca.paa";
-            tooltip = "Rushes to target position with no regard for safety";
+            tooltip = CSTRING(Waypoint_TaskAssault_ToolTip);
         };
         class EGVAR(danger,Retreat) {
-            displayName = "0 TASK FORCED RETREAT";
+            displayName = CSTRING(Waypoint_TaskRetreat_displayName);
             displayNameDebug = QEGVAR(danger,Retreat);
             file = QPATHTOF(scripts\fnc_wpRetreat.sqf);
             icon = "\a3\ui_f\data\GUI\Cfg\CommunicationMenu\attack_ca.paa";
-            tooltip = "Retreats to target position while ignoring enemies";
+            tooltip = CSTRING(Waypoint_TaskRetreat_ToolTip);
         };
         class EGVAR(danger,Garrison) {
-            displayName = "1 TASK GARRISON";
+            displayName = CSTRING(Waypoint_TaskGarrison_displayName);
             displayNameDebug = QEGVAR(danger,Garrison);
             file = QPATHTOF(scripts\fnc_wpGarrison.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Guard_ca.paa";
-            tooltip = "Garrisons in buildings around waypoint out to WP completion radius (default 50m)";
+            tooltip = CSTRING(Waypoint_TaskGarrison_ToolTip);
         };
         class EGVAR(danger,Patrol) {
-            displayName = "2 TASK PATROL";
+            displayName = CSTRING(Waypoint_TaskPatrol_displayName);
             displayNameDebug = QEGVAR(danger,Patrol);
             file = QPATHTOF(scripts\fnc_wpPatrol.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Loiter_ca.paa";
-            tooltip = "Randomised patrol within WP completion radius (Default 200m)\nNB will remove all existing waypoints from group";
+            ToolTip = CSTRING(Waypoint_TaskPatrol_ToolTip);
         };
         class EGVAR(danger,Rush) {
-            displayName = "3 TASK RUSH";
+            displayName = CSTRING(Waypoint_TaskRush_displayName);
             displayNameDebug = QEGVAR(danger,Rush);
             file = QPATHTOF(scripts\fnc_wpRush.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Sentry_ca.paa";
-            tooltip = "Rushes players within WP completion radius (Default 1000m)\nOnly targets players!";
+            tooltip = CSTRING(Waypoint_TaskRush_ToolTip);
         };
         class EGVAR(danger,Hunt) {
-            displayName = "4 TASK HUNT";
+            displayName = CSTRING(Waypoint_TaskHunt_displayName);
             displayNameDebug = QEGVAR(danger,Hunt);
             file = QPATHTOF(scripts\fnc_wpHunt.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Sentry_ca.paa";
-            tooltip = "Hunts players within WP completion radius (Default 1000m)\nOnly targets players!";
+            tooltip = CSTRING(Waypoint_TaskHunt_ToolTip);
         };
         class EGVAR(danger,Creep) {
-            displayName = "5 TASK CREEP";
+            displayName = CSTRING(Waypoint_TaskCreep_displayName);
             displayNameDebug = QEGVAR(danger,Creep);
             file = QPATHTOF(scripts\fnc_wpCreep.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Sentry_ca.paa";
-            tooltip = "Stalks players within WP completion radius (Default 1000m)\nOnly targets players!";
+            tooltip = CSTRING(Waypoint_TaskCreep_ToolTip);
         };
         class EGVAR(danger,CQB) {
-            displayName = "6 TASK CQB [Experimental]";
+            displayName = CSTRING(Waypoint_TaskCQB_displayName);
             displayNameDebug = QEGVAR(danger,CQB);
             file = QPATHTOF(scripts\fnc_wpCQB.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Scripted_ca.paa";
-            tooltip = "Enters and clears buildings within WP completion radius (Default 50m)";
+            tooltip = CSTRING(Waypoint_TaskCQB_ToolTip);
         };
         class EGVAR(danger,Artillery) {
-            displayName = "9 REGISTER ARTILLERY";
+            displayName = CSTRING(Waypoint_RegisterArtillery_displayName);
             displayNameDebug = "LAMBS_Register_Artillery";
             file = QPATHTOF(functions\fnc_taskArtilleryRegister.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Scripted_ca.paa";
-            tooltip = "Registers artillery for dynamic use";
+            tooltip = CSTRING(Waypoint_RegisterArtillery_ToolTip);
         };
     };
 };
