e8c9ce5e1ace48874c2fd0e8305fcb4e463f481c
jokoho482 <hoffmann.jonas95@gmail.com>
2021-05-07 21:00:18 +0200

    fix issues in stringtables

--- addons/wp/CfgWaypoints.hpp
+++ addons/wp/CfgWaypoints.hpp
@@ -6,63 +6,63 @@ class CfgWaypoints {
             displayNameDebug = QEGVAR(danger,Attack);
             file = QPATHTOF(scripts\fnc_wpAssault.sqf);
             icon = "\a3\ui_f\data\GUI\Cfg\CommunicationMenu\attack_ca.paa";
-            tooltip = CSTRING(Waypoint_TaskAssault_ToolTip);
+            tooltip = CSTRING(Waypoint_TaskAssault_Tooltip);
         };
         class EGVAR(danger,Retreat) {
             displayName = CSTRING(Waypoint_TaskRetreat_displayName);
             displayNameDebug = QEGVAR(danger,Retreat);
             file = QPATHTOF(scripts\fnc_wpRetreat.sqf);
             icon = "\a3\ui_f\data\GUI\Cfg\CommunicationMenu\attack_ca.paa";
-            tooltip = CSTRING(Waypoint_TaskRetreat_ToolTip);
+            tooltip = CSTRING(Waypoint_TaskRetreat_Tooltip);
         };
         class EGVAR(danger,Garrison) {
             displayName = CSTRING(Waypoint_TaskGarrison_displayName);
             displayNameDebug = QEGVAR(danger,Garrison);
             file = QPATHTOF(scripts\fnc_wpGarrison.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Guard_ca.paa";
-            tooltip = CSTRING(Waypoint_TaskGarrison_ToolTip);
+            tooltip = CSTRING(Waypoint_TaskGarrison_Tooltip);
         };
         class EGVAR(danger,Patrol) {
             displayName = CSTRING(Waypoint_TaskPatrol_displayName);
             displayNameDebug = QEGVAR(danger,Patrol);
             file = QPATHTOF(scripts\fnc_wpPatrol.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Loiter_ca.paa";
-            ToolTip = CSTRING(Waypoint_TaskPatrol_ToolTip);
+            ToolTip = CSTRING(Waypoint_TaskPatrol_Tooltip);
         };
         class EGVAR(danger,Rush) {
             displayName = CSTRING(Waypoint_TaskRush_displayName);
             displayNameDebug = QEGVAR(danger,Rush);
             file = QPATHTOF(scripts\fnc_wpRush.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Sentry_ca.paa";
-            tooltip = CSTRING(Waypoint_TaskRush_ToolTip);
+            tooltip = CSTRING(Waypoint_TaskRush_Tooltip);
         };
         class EGVAR(danger,Hunt) {
             displayName = CSTRING(Waypoint_TaskHunt_displayName);
             displayNameDebug = QEGVAR(danger,Hunt);
             file = QPATHTOF(scripts\fnc_wpHunt.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Sentry_ca.paa";
-            tooltip = CSTRING(Waypoint_TaskHunt_ToolTip);
+            tooltip = CSTRING(Waypoint_TaskHunt_Tooltip);
         };
         class EGVAR(danger,Creep) {
             displayName = CSTRING(Waypoint_TaskCreep_displayName);
             displayNameDebug = QEGVAR(danger,Creep);
             file = QPATHTOF(scripts\fnc_wpCreep.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Sentry_ca.paa";
-            tooltip = CSTRING(Waypoint_TaskCreep_ToolTip);
+            tooltip = CSTRING(Waypoint_TaskCreep_Tooltip);
         };
         class EGVAR(danger,CQB) {
             displayName = CSTRING(Waypoint_TaskCQB_displayName);
             displayNameDebug = QEGVAR(danger,CQB);
             file = QPATHTOF(scripts\fnc_wpCQB.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Scripted_ca.paa";
-            tooltip = CSTRING(Waypoint_TaskCQB_ToolTip);
+            tooltip = CSTRING(Waypoint_TaskCQB_Tooltip);
         };
         class EGVAR(danger,Artillery) {
             displayName = CSTRING(Waypoint_RegisterArtillery_displayName);
             displayNameDebug = "LAMBS_Register_Artillery";
             file = QPATHTOF(functions\fnc_taskArtilleryRegister.sqf);
             icon = "\a3\3DEN\Data\CfgWaypoints\Scripted_ca.paa";
-            tooltip = CSTRING(Waypoint_RegisterArtillery_ToolTip);
+            tooltip = CSTRING(Waypoint_RegisterArtillery_Tooltip);
         };
     };
 };
