dd0b8a846facb5ac3c0ab6419e11c3086dfaac13
jokoho482 <hoffmann.jonas95@gmail.com>
2021-06-20 22:13:57 +0200

    add First Concept Idea for User Configurable Sensitivity Settings

--- addons/range/CfgVehicles.hpp
+++ addons/range/CfgVehicles.hpp
@@ -1,6 +1,6 @@
 class CfgVehicles {
     class Land;
     class Man : Land {
-        sensitivity = 6;
+        sensitivity = LAMBS_RANGE_SENSITIVITY_MAN;
     };
 };
--- addons/range/config.cpp
+++ addons/range/config.cpp
@@ -11,4 +11,14 @@ class CfgPatches {
     };
 };
 
+class DummyClass {
+    #if __has_include("\userconfig\lambs_danger\range.hpp")
+        #include "\userconfig\lambs_danger\range.hpp";
+    #endif
+
+    #ifndef LAMBS_RANGE_SENSITIVITY_MAN
+        #define LAMBS_RANGE_SENSITIVITY_MAN 6
+    #endif
+};
+
 #include "CfgVehicles.hpp"
