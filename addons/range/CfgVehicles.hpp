ac6aa699d7c3d32442adb80bbd6852f3d7687e8b
rekterakathom <30532050+rekterakathom@users.noreply.github.com>
2024-07-07 17:49:44 +0300

    Switch to CAManBase

--- addons/range/CfgVehicles.hpp
+++ addons/range/CfgVehicles.hpp
@@ -1,16 +1,6 @@
 class CfgVehicles {
-    class CAManBase;
-    class Civilian;
-    class SoldierWB: CAManBase {
-        sensitivity = 6;
-    };
-    class SoldierEB: CAManBase {
-        sensitivity = 6;
-    };
-    class SoldierGB: CAManBase {
-        sensitivity = 6;
-    };
-    class Civilian_F: Civilian {
+    class Man;
+    class CAManBase: Man {
         sensitivity = 6;
     };
 };
